187fd80814751ca4aaf3bbb3197da86ef84ee8dd
Rob Scherer <robscherer6@gmail.com>
2023-07-05 07:21:36 -0400

    sample fun facts about react page created

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const page = (
   <div>
-    <h1 className='header'>eatWhere</h1>
+    <img src="./react-logo.png" alt="" width='40px'/>
+    <h1>Fun facts about React</h1>
     <ul>
-      <li>About</li>
-      <li>Pricing</li>
-      <li>Contact</li>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
   </div>
 )
+
+// const page = (
+//   <div>
+//     <h1 className='header'>eatWhere</h1>
+//     <ul>
+//       <li>About</li>
+//       <li>Pricing</li>
+//       <li>Contact</li>
+//     </ul>
+//   </div>
+// )
 
 ReactDOM.render(
   page,
