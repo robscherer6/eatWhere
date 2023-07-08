f4742e4256401f3cc5f01b8c19bcbc64e43f0a4d
Rob Scherer <robscherer6@gmail.com>
2023-07-08 19:53:01 -0400

    highlighting the limitations of hard coded data

--- eatwhere/src/components/Card.js
+++ eatwhere/src/components/Card.js
@@ -1,11 +1,17 @@
 import Food from '../images/food.png'
+import Star from '../images/star.png'
 function Card() {
   return (
     <div className="card">
-      <img src={Food} alt="food" className="food-image"/>
-      <h6>Rating</h6>
-      <h6>Title</h6>
-      <h6>Price</h6>
+      <img src={Food} alt="food" className="card-image"/>
+      <div className="card-stats">
+        <img src={Star} alt="star" className="card--star"/>
+        <span>5.0</span>
+        <span>(6) • </span>
+        <span>Frederick, MD</span>
+      </div>
+      <p>Lazy Fish</p>
+      <p><span className="bold">$$</span></p>
     </div>
   )
 }
