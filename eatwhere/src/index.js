44d5bfa0d81ddaa865d359d8efda690c72656329
Rob Scherer <robscherer6@gmail.com>
2023-07-07 21:45:42 -0400

    component and navbar added using components and top-level app

--- eatwhere/src/index.js
+++ eatwhere/src/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
+//import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles.css'
+import App from './App'
 
 ReactDOM.render (
-  <h1>Hello, Create React App</h1>,
+  <App />,
   document.getElementById('root')
 )
