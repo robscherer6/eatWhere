f716849221024623914a531124014b10c175c35a
Rob Scherer <robscherer6@gmail.com>
2023-07-08 12:05:43 -0400

    styled main content

--- eatwhere/src/components/Main.js
+++ eatwhere/src/components/Main.js
@@ -1,6 +1,14 @@
 function Main() {
   return (
-    <h1>Main</h1>
+    <main>
+      <h1 className="main--title">Recommendations</h1>
+      <ul className='list'>
+        <li>Mood</li>
+        <li>Ambiance</li>
+        <li>Distance</li>
+        <li>Type of Food</li>
+      </ul>
+    </main>
   )
 }
 
