ad9d04c0f00b395435f1ba853c78a3c3dd3b7a3f
Rob Scherer <robscherer6@gmail.com>
2023-07-09 09:34:08 -0400

    leveraging props for contact info for different restaurants

--- eatwhere/src/App.js
+++ eatwhere/src/App.js
@@ -2,14 +2,40 @@ import Navbar from './components/Navbar'
 import Main from './components/Main'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import Contact from './components/Contact'
 
 function App() {
+
   return (
     <div>
       <Navbar />
       <Hero />
       <Main />
       <Card />
+      <Contact
+        img="./images/lazyFish.png"
+        name="Lazy Fish"
+        phone="(240) 372-1234"
+        email="lazyfish@lazy.com"
+      />
+      <Contact
+        img="./images/lazyFish.png"
+        name="Summitra"
+        phone="(555) 555-1234"
+        email="summitra@summitra.com"
+      />
+      <Contact
+        img="./images/lazyFish.png"
+        name="McDonalds"
+        phone="(123) 555-1234"
+        email="mcdonalds@mcdonalds.com"
+      />
+      <Contact
+        img="./images/lazyFish.png"
+        name="Chipotle"
+        phone="(555) 123-1234"
+        email="chipotle@chipotle.com"
+      />
     </div>
   )
 }
---
+++ eatwhere/src/components/Contact.js
@@ -0,0 +1,18 @@
+function Contact(props) {
+  return (
+    <div className="contact-card">
+      <img src={props.img} alt="" />
+      <h3>{props.name}</h3>
+      <div className="info-group">
+        <img src="" alt="phone" />
+        <p>{props.phone}</p>
+      </div>
+      <div className="info-group">
+        <img src="" alt="email" />
+        <p>{props.email}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
