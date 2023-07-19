0da9043f2f1756939e225826150c1c618edf4577
Rob Scherer <robscherer6@gmail.com>
2023-07-19 16:19:17 -0400

    testing fetch api call insdie app component

--- eatwhere/src/index.js
+++ eatwhere/src/index.js
@@ -7,3 +7,22 @@ ReactDOM.render (
   <App />,
   document.getElementById('root')
 )
+
+const fetch = require('node-fetch');
+
+const url = 'https://the-fork-the-spoon.p.rapidapi.com/sale-type-menu/list?id_restaurant=522995&locale=en_US';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '712b47486fmsh49a364b513ca73dp1d0a33jsn7b21a5dd1328',
+    'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
