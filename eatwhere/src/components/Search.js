86407f7a04573a785296c980ee920bb0ef48ef29
Rob Scherer <robscherer6@gmail.com>
2023-08-03 16:09:08 -0400

    adding fetch request to API to retrieve dummy data

--- eatwhere/src/components/Search.js
+++ eatwhere/src/components/Search.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Search () {
   const [isOpenOnMonday, setIsOpenOnMonday] = React.useState("Yes")
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
         
   function handleClick(e) {
     e.preventDefault();
-    if (setIsOpenOnMonday("No")) {
-      setIsOpenOnMonday("Yes")
-    }
+    fetchData();
   }
 
   return (
@@ -22,3 +38,20 @@ function Search () {
 }
 
 export default Search
+
+function MyComponent() {
+  const [data, setData] = useState([]);
+
+
+
+  return (
+    <div>
+      <h1>API Data</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
