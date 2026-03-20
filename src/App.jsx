fb7aefc6144dba084b19fcf20d1b2301723b7330
Rob Scherer <robscherer6@gmail.com>
2026-03-19 20:44:59 -0400

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <>
-      <div>eatWhere</div>
-    </>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-orange-500">eatWhere</h1>
+    </div>
   )
 }
 
