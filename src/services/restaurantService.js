9836e115b1377439aa1cf42a0c4312bd7103f81d
Rob Scherer <robscherer6@gmail.com>
2026-03-20 10:01:26 -0400

    added fetch request, really a post with json data

--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -4,4 +4,34 @@ const BASE_URL = 'https://places.googleapis.com/v1'
 export const searchNearbyRestaurants = async (location, filters = {}) => {
     const { cuisine = '', distance = 5000 } = filters
 
+    const body = {
+        includedTypes: cuisine ? [cuisine] : ['restaurant'],
+        maxResultCount: 20,
+        locationRestriction: {
+            circle: {
+                center: {
+                    latitude: location.lat,
+                    longitude: location.lng
+                },
+                radius: distance
+            }
+        }
+    }
+
+    const response = await fetch(`${BASE_URL}/places:searchNearby`, {
+        headers: {
+            'Content-Type' : 'application/json',
+            'X-Goog-Api-Key' : API_KEY,
+            'X-Goog-FieldMask': 'places.id,places.displayName,places.rating,places.userRatingCount,places.formattedAddress,places.types,places.regularOpeningHours,places.photos'
+        },
+        body: JSON.stringify(body)
+    })
+
+    if (!response.ok) {
+        throw new Error('failed to fetch restaurant data')
+    }
+
+    const data = await response.json()
+    return data.places || []
+
 }
