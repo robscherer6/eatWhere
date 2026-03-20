6a47b855a2db73404767a4518c90f7ffeb14551b
Rob Scherer <robscherer6@gmail.com>
2026-03-20 15:37:01 -0400

    cleaning up test code

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,7 @@ import { RestaurantProvider } from "./context/RestaurantContext";
 import { geocodeLocation, searchNearbyRestaurants } from "./services/restaurantService";
 
 function App() {
-  useEffect(() => {
-    const testAPI = async () => {
-      try {
-        console.log('testing geocoding...');
-        const location = await geocodeLocation('Middletown, MD');
-        console.log('location: ', location);
-
-        console.log('testing restaurant search nearby...');
-        const restaurants = await searchNearbyRestaurants(location, {
-          cuisine: '',
-          distance: 5000
-        })
-        console.log('restaurants found: ', restaurants);
-      } catch (error) {
-        console.error('API error', error)
-      }
-    }
-    testAPI()
-  }, [])
-
+  
   return (
     <RestaurantProvider>
       <div className="flex items-center justify-center h-screen bg-gray-100">
