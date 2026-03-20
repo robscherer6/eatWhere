375d825856804baf7e575ae2ad3810846004aa31
Rob Scherer <robscherer6@gmail.com>
2026-03-20 13:54:53 -0400

    added restaurant service layer leveraging google places and geocode apis

--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -1,37 +1,54 @@
-const API_KEY =  import.meta.env.VITE_GOOGLE_PLACES_API_KEY
-const BASE_URL = 'https://places.googleapis.com/v1'
+const API_KEY = import.meta.env.VITE_GOOGLE_PLACES_API_KEY;
+const BASE_URL = "https://places.googleapis.com/v1";
 
 export const searchNearbyRestaurants = async (location, filters = {}) => {
-    const { cuisine = '', distance = 5000 } = filters
+  const { cuisine = "", distance = 5000 } = filters;
 
-    const body = {
-        includedTypes: cuisine ? [cuisine] : ['restaurant'],
-        maxResultCount: 20,
-        locationRestriction: {
-            circle: {
-                center: {
-                    latitude: location.lat,
-                    longitude: location.lng
-                },
-                radius: distance
-            }
-        }
-    }
-
-    const response = await fetch(`${BASE_URL}/places:searchNearby`, {
-        headers: {
-            'Content-Type' : 'application/json',
-            'X-Goog-Api-Key' : API_KEY,
-            'X-Goog-FieldMask': 'places.id,places.displayName,places.rating,places.userRatingCount,places.formattedAddress,places.types,places.regularOpeningHours,places.photos'
+  const body = {
+    includedTypes: cuisine ? [cuisine] : ["restaurant"],
+    maxResultCount: 20,
+    locationRestriction: {
+      circle: {
+        center: {
+          latitude: location.lat,
+          longitude: location.lng,
         },
-        body: JSON.stringify(body)
-    })
+        radius: distance,
+      },
+    },
+  };
 
-    if (!response.ok) {
-        throw new Error('failed to fetch restaurant data')
-    }
+  const response = await fetch(`${BASE_URL}/places:searchNearby`, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Goog-Api-Key": API_KEY,
+      "X-Goog-FieldMask":
+        "places.id,places.displayName,places.rating,places.userRatingCount,places.formattedAddress,places.types,places.regularOpeningHours,places.photos",
+    },
+    body: JSON.stringify(body),
+  });
 
-    const data = await response.json()
-    return data.places || []
+  if (!response.ok) {
+    throw new Error("failed to fetch restaurant data");
+  }
 
-}
+  const data = await response.json();
+  return data.places || [];
+};
+
+export const geocodeLocation = async (locationString) => {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(locationString)}&key=${API_KEY}`,
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to geocode location");
+  }
+
+  const data = response.json();
+  if (data.results.length === 0) {
+    throw new Error("Location not found");
+  }
+
+  return data.results[0].geometry.location;
+};
